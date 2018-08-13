d1d9707406d53e552b77ebf84cea0a484a29817d
David Stockhouse <dstockhouse@gmail.com>
2018-08-12 21:30:13 -0700

    Cleaned up and refined new latch.

--- src/pl/new_latch.vhd
+++ src/pl/new_latch.vhd
@@ -57,39 +57,6 @@ architecture Behavioral of new_latch is
 
 	------ External component declarations ------
 
--- 	component DDRlatch is
--- 		Port ( d : in STD_LOGIC;
--- 		       latch : in STD_LOGIC;
--- 		       clk : in STD_LOGIC;
--- 		       rst : in STD_LOGIC;
--- 		       q : out STD_LOGIC_VECTOR (9 downto 0));
--- 	end component;
--- 
--- 	component pll_wrapper is
--- 		Port ( clk : in STD_LOGIC;
--- 		       rst : in STD_LOGIC;
--- 		       clk_out : out STD_LOGIC_VECTOR ( 5 downto 0 );
--- 		       locked : out STD_LOGIC);
--- 	end component;
--- 
--- 	component match is
--- 		Port ( test : in STD_LOGIC_VECTOR (9 downto 0);
--- 		       key : in STD_LOGIC_VECTOR (9 downto 0);
--- 		       train_en : in STD_LOGIC;
--- 		       rst : in STD_LOGIC;
--- 		       matched : out STD_LOGIC;
--- 		       shifted : out STD_LOGIC_VECTOR (9 downto 0);
--- 		       shift : out INTEGER);
--- 	end component;
--- 
--- 	component clock_select is
--- 		Port ( clk_sel : in STD_LOGIC_VECTOR (5 downto 0);
--- 		       train_en : in STD_LOGIC;
--- 		       rst : in STD_LOGIC;
--- 		       chosen : out integer;
--- 		       confident : out STD_LOGIC);
--- 	end component;
-
 	component DDRshift is
 		Generic ( bits : integer);
 		Port ( d : in STD_LOGIC;
@@ -102,40 +69,13 @@ architecture Behavioral of new_latch is
 
 	------ Internal signal declarations ------
 
-	-- Clock signals that are each out of phase a certain amount from the
-	-- main clock
-	signal phase_clk : std_logic_vector(5 downto 0);
-
-	-- Internal shift register latch signal and inverted clock
-	signal int_latch, inv_clk, pll_locked : std_logic;
-
-	-- The type phase_set is an array of ten bit vectors representing the
-	-- six words shifted in by each phase clock
-	type phase_set is array (0 to 5) of std_logic_vector (9 downto 0);
-	signal internal : phase_set := (others => (others => '0'));
-	signal internal_shifted : phase_set := (others => (others => '0'));
-
-	-- clk_sel is a boolean representing which phase clocks have
-	-- the correct training data, even if it's shifted a few bits
-	signal clk_sel : std_logic_vector(0 to 5) := (others => '0');
-
-	-- clk_choice is which of the phase clocks is selected as the
-	-- correct phase for the input line
-	signal clk_choice : integer := 0;
-
-	-- sel is an array of integers that keeps track of which
-	-- phased clocks shifted the correct data and if they are
-	-- a discrete number of bits out of sync
-	-- shift_sel keeps track of how many bits the correctly 
-	-- phased clocks are out of sync
-	type sel is array (0 to 5) of integer;
-	signal shift_sel : sel := (others => 0);
-
-	-- qtemp is the 10-bit output latched from the serial input line
-	signal qtemp : std_logic_vector (9 downto 0);
-
+	-- Internal 30-bit buffers from the serial data output from the sensor
 	signal int_q1, int_q2, int_ctl : std_logic_vector (29 downto 0);
+
+	-- Offset locations from which to draw each pixel from the 30-bit buffer
 	signal offset1, offset2, offset_ctl : integer := 0;
+
+	-- Internal signal for whether or not the device is currently training
 	signal int_train : std_logic := '1';
 
 begin
@@ -162,13 +102,21 @@ begin
 			  rst => rst,
 			  q => int_ctl);
 
+
+	-- Process to determine where the correct position of each pixel data in
+	-- the longer buffer whenever the pixel clock rising edge occurs
 	FRAMING : process(pix_clk, rst)
+		-- Starting positions in each buffer
 		variable pos1, pos2, pos_ctl : integer;
+		-- Difference between positions to determine whether to
+		-- reposition certain signals
 		variable diff_1_2, diff_1_ctl, diff_2_ctl : integer;
+		-- Bits in the control channel
 		variable dval, fval, lval : std_logic;
 	begin
 
 		if rst = '1' then
+			-- Reset signals and variables
 
 			-- Pixel delineation/framing
 			pos1 := -1;
@@ -193,13 +141,17 @@ begin
 
 			if int_train = '1' then
 
-				-- Reset position variables
+				-- Reset position variables so it can be
+				-- determined whether or not they have been
+				-- properly set later
 				pos1 := -1;
 				pos2 := -1;
 				pos_ctl := -1;
 
-				-- Determine framing for each signal
-				for I in 0 to 9 loop
+				-- Determine first possible frame for each signal
+				INIT_FRAME_LOOP : for I in 0 to 9 loop
+					-- Test each signal buffer against it's
+					-- training sequence
 					if int_q1((I+9) downto I) = "0001010101" then
 						pos1 := I;
 					end if;
@@ -209,7 +161,7 @@ begin
 					if int_ctl((I+9) downto I) = "1000000000" then
 						pos_ctl := I;
 					end if;
-				end loop;
+				end loop; -- INIT_FRAME_LOOP
 
 				-- Determine if positioning would be closer at a
 				-- different offset
@@ -271,8 +223,8 @@ begin
 
 					locked <= '1';
 
-					-- Because int_train is more heavily 
-					-- dependent on the switching of 
+					-- Because int_train is more directly
+					-- dependent on the switching of
 					-- train_en in the next process, at this
 					-- point the signal is only driven
 					-- recessively
@@ -323,106 +275,5 @@ begin
 			int_train <= '0';
 		end if;
 	end process; -- TRAIN_PROC
-
-
--- 	-- Inverted clock
--- 	inv_clk <= '0' when rst = '1' else
--- 		   not clk;
--- 
--- 
--- 	-- Clock phase generating PLL block. Wrapper around block design
--- 	PLL_INST : pll_wrapper port map(clk => clk,
--- 					rst => rst,
--- 					clk_out => phase_clk,
--- 					locked => pll_locked);
--- 
--- 	-- Setup six ten-bit latched DDR shift registers (from DDRlatch.vhd)
--- 	SHIFT_CHANNELS : for I in 0 to 5 generate
--- 
--- 		-- A single data line
--- 		SHIFT_GEN : DDRlatch port map(d => d,
--- 					      clk => phase_clk(I),
--- 					      rst => rst,
--- 					      latch => int_latch,
--- 					      q => internal(I));
--- 
--- 	end generate SHIFT_CHANNELS;
--- 
--- 
--- 	-- Count to ten on the DDR clock and set the internal latch, then
--- 	-- check the internal signals to see if they match the training
--- 	-- data
--- 	COUNT_PROC : process(clk, inv_clk, rst)
--- 
--- 		-- Counter variable, initialize to 0
--- 		variable count : integer := 0;
--- 
--- 	begin
--- 
--- 		-- Check reset signal, set all signals to 0
--- 		if rst = '0' then
--- 
--- 			-- Check for rising or falling edge (rising on inv_clk
--- 			if rising_edge(clk) or rising_edge(inv_clk) then
--- 
--- 				-- Increment counter
--- 				count := count + 1;
--- 
--- 				-- If latch is set, reset it
--- 				if(clk = '1' and int_latch = '1') then
--- 					int_latch <= '0';
--- 				end if;
--- 
--- 				-- If count reached 10, set internal latch
--- 				if(count >= 10) then
--- 					if(clk = '1') then
--- 						int_latch <= '1';
--- 					end if;
--- 					count := 0;
--- 				end if;
--- 
--- 			end if; -- DDR edge detector
--- 
--- 		else
--- 
--- 			int_latch <= '0';
--- 			out_latch <= '0';
--- 			count := 0;
--- 
--- 		end if; -- rst
--- 
--- 	end process; -- COUNT_PROC
--- 
--- 
--- 	-- Generate the training data comparison blocks
--- 	COMPARE : for I in 0 to 5 generate
--- 
--- 		SHIFT_COMPARE : match port map (test => internal(I),
--- 						key => train,
--- 						train_en => train_en,
--- 						rst => rst,
--- 						matched => clk_sel(I),
--- 						shifted => internal_shifted(I),
--- 						shift => shift_sel(I));
--- 
--- 	end generate COMPARE;
--- 
--- 
--- 	-- Select the right clock based on the value of clk_sel
--- 	FINAL_SELECTION : clock_select port map (clk_sel => clk_sel,
--- 						 train_en => train_en,
--- 						 rst => rst,
--- 						 chosen => clk_choice,
--- 						 confident => locked);
--- 
--- 
--- 	-- Assign output to the properly selected and shifted input phase
--- 	qtemp <= (others => '0') when rst = '1' or (clk_choice < 0 or clk_choice > 5) else
--- 		 internal_shifted(clk_choice);
--- 		 -- to_stdlogicvector(to_bitvector(internal(clk_choice)) rol conv_integer(shift_sel(clk_choice)));
--- 
--- 	-- Trim the 2 LSBs for the final latched output
--- 	q <= (others => '0') when rst = '1' else
--- 	     qtemp (9 downto 2);
 
 end Behavioral;
